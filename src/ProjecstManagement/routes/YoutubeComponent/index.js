ef329a5668491da9d10d29d62a630f136293b415
jayachandra gopavaram <jayachandra572@gmail.com>
2020-11-28 19:42:08 +0530

    auto ply

--- src/ProjecstManagement/routes/YoutubeComponent/index.js
+++ src/ProjecstManagement/routes/YoutubeComponent/index.js
@@ -25,7 +25,7 @@ class YoutubeComponent extends Component {
                height='480'
                id={iframeID}
                style={{ margin: '20px' }}
-               src={iframeSrc+'?autoplay=1&mute=1'}
+               src={'src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1'}
                frameBorder='0'
                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
                allowFullScreen
